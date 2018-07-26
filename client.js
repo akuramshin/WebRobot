7b77edbda49b022d01a2e2135a865fdc7585157d
Artur Kuramshin <artur2600artur@gmail.com>
2018-07-26 17:39:06 -0400

    Added change mode dropdown

--- client.js
+++ client.js
@@ -16,3 +16,24 @@ function sendMove(dir) {
 	// stop link reloading the page
 	event.preventDefault();
 }
+
+/* When the user clicks the button,
+toggle between hiding and showing the dropdown content */
+function dropDown(){
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+	    var myDropdown = document.getElementById("myDropdown");
+	    if (myDropdown.classList.contains('show')){
+	        myDropdown.classList.remove('show');
+	    }
+	}
+}
+
+// Function that changes robot mode and sends info to server
+function changeMode(mode) {
+	console.log("Changed mode to " + mode);
+}
