9af20470417ea1ce431544a70afa498533aa2714
Artur Kuramshin <artur2600artur@gmail.com>
2018-08-07 16:18:07 -0400

    Added a login page for the website

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request, redirect, Response
+from flask import Flask, render_template, request, redirect, Response, session, abort
 import random, json
 import sys
 import socket
 
 app = Flask(__name__)
+
+app.secret_key = 'X;\xce\xcc\xde\x8f.\x117\x16tO\xfd\x98\n<'
+
 # Add a feature to index.html where we can see if bot is connected or not ...
 # (if the socket client connected with the server running on the bot )
-
 HOST = "192.168.0.55"
 PORT = 9999
 
@@ -30,9 +32,23 @@ def relay(msg):
 
 @app.route("/")
 def index():
-	# Return our main page
-    return render_template('index.html')
+	if not session.get('logged_in'):
+		# Return the log in page
+		return render_template('login.html')
+	else:
+		# Return our main page
+		return render_template('index.html')
 
+
+@app.route("/login", methods = ['POST', 'GET'])
+def do_login():
+	if request.method == 'POST':
+		if request.form['password'] == 'password' and request.form['username'] == 'admin':
+			session['logged_in'] = True
+		else:
+			flash('Wrong Password!')
+		return index()
+	return render_template('login.html')
 
 @app.route("/move", methods = ['POST'])
 def recieve():
