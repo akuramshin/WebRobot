0d7f80e0c7bb5f14af9defad56ff4e27ff9f3ee3
Artur <artur.kuramshin@mail.utoronto.ca>
2020-04-11 18:03:15 -0400

    Combining speach and video

--- robot/robotInterface.py
+++ robot/robotInterface.py
@@ -9,11 +9,11 @@ import speech_recognition as sr
 import os
 
 # Initialize our socket to recieve commands
-##sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-##sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-##sock.bind(('', 9999))
-##sock.listen(1)
-##print("Listening for connections...")
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('', 9999))
+sock.listen(1)
+print("Listening for connections...")
 
 # Create out bot connection
 bot = create2api.Create2()
@@ -52,41 +52,43 @@ def changeMode(mode):
 		bot.start()
 		bot.seek_dock()
 
-##try:
-##	while True:
-##		conn, addr = sock.accept()
-##		print("Connected " + str(addr[0]) + ":" + str(addr[1]))
-##
-##		while True:
-##			try:
-##				data = conn.recv(1024).decode()
-##			except Exception:
-##				break
-##			if data:
-##				print(data)
-##			else:
-##				break
-##			if data[0:2] == "MO":
-##				# We are moving
-##				direction = data[2:data.index("S")]
-##				speed = data[data.index("S")+1:]
-##				sendCmd(direction, int(speed))
-##
-##			elif data[0:2] == "MD":
-##				# We are changing modes
-##				mode = data[2:]
-##				changeMode(mode)
-##
-##		print("Disconnected " + str(addr[0]) + ":" + str(addr[1]))
-##		conn.close()
-##		bot.stop()
-##
-##except Exception:
-##	print("Socket terminated")
-##	sock.shutdown(socket.SHUT_RDWR)
-##	sock.close()
+try:
+	while True:
+		conn, addr = sock.accept()
+		print("Connected " + str(addr[0]) + ":" + str(addr[1]))
+
+		voice()
+
+		while True:
+			try:
+				data = conn.recv(1024).decode()
+			except Exception:
+				break
+			if data:
+				print(data)
+			else:
+				break
+			if data[0:2] == "MO":
+				# We are moving
+				direction = data[2:data.index("S")]
+				speed = data[data.index("S")+1:]
+				sendCmd(direction, int(speed))
+
+			elif data[0:2] == "MD":
+				# We are changing modes
+				mode = data[2:]
+				changeMode(mode)
+
+		print("Disconnected " + str(addr[0]) + ":" + str(addr[1]))
+		conn.close()
+		bot.stop()
+
+except Exception:
+	print("Socket terminated")
+	sock.shutdown(socket.SHUT_RDWR)
+	sock.close()
 def audioRecorderCallback(fname):
-    print "converting audio to text"
+    print ("converting audio to text")
     r = sr.Recognizer()
     with sr.AudioFile(fname) as source:
         audio = r.record(source)  # read the entire audio file
@@ -102,13 +104,11 @@ def audioRecorderCallback(fname):
         elif resp == "stop":
                 bot.stop()
     except sr.UnknownValueError:
-        print "Google Speech Recognition could not understand audio"
+        print ("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
-        print "Could not request results from Google Speech Recognition service; {0}".format(e)
+        print ("Could not request results from Google Speech Recognition service; {0}".format(e))
 
     os.remove(fname)
-
-
 
 def detectedCallback():
     bot.play_test_sound()
@@ -119,29 +119,29 @@ def signal_handler(signal, frame):
     global interrupted
     interrupted = True
 
-
 def interrupt_callback():
     global interrupted
     return interrupted
 
-if len(sys.argv) == 1:
-    print "Error: need to specify model name"
-    print "Usage: python demo.py your.model"
-    sys.exit(-1)
+def voice():
+	if len(sys.argv) == 1:
+		print ("Error: need to specify model name")
+		print ("Usage: python demo.py your.model")
+		sys.exit(-1)
 
-model = sys.argv[1]
+	model = sys.argv[1]
 
-# capture SIGINT signal, e.g., Ctrl+C
-signal.signal(signal.SIGINT, signal_handler)
+	# capture SIGINT signal, e.g., Ctrl+C
+	signal.signal(signal.SIGINT, signal_handler)
 
-detector = snowboydecoder.HotwordDetector(model, sensitivity=0.38)
-print "Listening... Press Ctrl+C to exit"
+	detector = snowboydecoder.HotwordDetector(model, sensitivity=0.38)
+	print ("Listening... Press Ctrl+C to exit")
 
-# main loop
-detector.start(detected_callback=detectedCallback,
-               audio_recorder_callback=audioRecorderCallback,
-               interrupt_check=interrupt_callback,
-               sleep_time=0.01)
+	# main loop
+	detector.start(detected_callback=detectedCallback,
+				audio_recorder_callback=audioRecorderCallback,
+				interrupt_check=interrupt_callback,
+				sleep_time=0.01)
 
-detector.terminate()
-bot.stop()
+	detector.terminate()
+	bot.stop()
